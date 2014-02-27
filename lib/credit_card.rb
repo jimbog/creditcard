99f6f3a342e085054e917e7516d2c9f3e935f716
jimbog <jimmygarzon@gmail.com>
2014-02-27 14:31:46 -0500

    replace variable name credit_line for credit_limit

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,11 +1,11 @@
 require_relative 'luhn10'
 
 class CreditCard
-  attr_reader :name, :number, :credit_line, :balance
+  attr_reader :name, :number, :credit_limit, :balance
   def initialize options = {}
     @name = options[:name]
     @number = options[:number]
-    @credit_line = options[:credit_line] || 0
+    @credit_limit = options[:credit_limit] || 0
     @balance =  0
   end
 
@@ -15,7 +15,7 @@ class CreditCard
   end
 
   def charge(amount)
-      if @balance + amount > @credit_line
+      if @balance + amount > @credit_limit
         #raise error
       else
         @balance += amount
--- spec/lib/credit_card_spec.rb
+++ spec/lib/credit_card_spec.rb
@@ -3,14 +3,13 @@ require_relative '../../lib/credit_card'
 
 describe CreditCard do
   it "is initialized with a hash" do
-    cc = CreditCard.new name: "Tom", number: 4111111111111111, credit_line: 1000
+    cc = CreditCard.new name: "Tom", number: 4111111111111111, credit_limit: 1000
     expect(cc.name).to eq("Tom")
     expect(cc.number).to eq(4111111111111111)
-    expect(cc.balance).to eq(0)
   end
 
-  it "is initialized with a balance of 0 if no credit line is passed in" do
-    cc = CreditCard.new name: "Tom", number: 4111111111111111
+  it "is initialized with a balance of 0" do
+    cc = CreditCard.new name: "Tom", number: 4111111111111111, credit_limit: 1000
     expect(cc.balance).to eq(0)
   end
 
@@ -25,27 +24,27 @@ describe CreditCard do
   end
 
   it "increases the balance by the amount of charges" do
-    cc = CreditCard.new name: "Tom", number: 4111111111111111, credit_line: 1000
+    cc = CreditCard.new name: "Tom", number: 4111111111111111, credit_limit: 1000
     cc.charge 200
     cc.charge 100
     expect(cc.balance).to eq(300)
   end
 
   it "ignores charges beyond the credit limit" do
-    cc = CreditCard.new name: "Tom", number: 4111111111111111, credit_line: 1000
+    cc = CreditCard.new name: "Tom", number: 4111111111111111, credit_limit: 1000
     cc.charge 500
     cc.charge 800
     expect(cc.balance).to eq(500)
   end
 
   it "decreases the balance by the amount of credits" do
-    cc = CreditCard.new name: "Lisa", number: 5454545454545454, credit_line: 3000
+    cc = CreditCard.new name: "Lisa", number: 5454545454545454, credit_limit: 3000
     cc.credit 100
     expect(cc.balance).to eq(-100)
   end
 
   it "handles both credit and charges to the same card" do
-    cc = CreditCard.new name: "Lisa", number: 5454545454545454, credit_line: 3000
+    cc = CreditCard.new name: "Lisa", number: 5454545454545454, credit_limit: 3000
     cc.credit 100
     cc.charge 7
     expect(cc.balance).to eq(-93)
